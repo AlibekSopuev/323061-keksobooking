bac3d1ba04c3e90294b811efcd7d19eac5fb3c32
Alibek <alibek92@mail.ru>
2018-04-10 20:30:01 +0600

    Правки

--- js/map.js
+++ js/map.js
@@ -10,12 +10,12 @@ function extractRandom(list) {
   return list.splice(index, 1).shift();
 }
 
-var avatars = [0,1,2,3,4,5,6,7,8];
-var titles = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
-var types = ["palace", "flat", "house", "bungalo"];
-var checkins = ["12:00", "13:00", "14:00"];
-var featuresOf = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var photosOf = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var avatars = [0, 1, 2, 3, ,5, 6, 7, 8];
+var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var types = ['palace', 'flat', 'house', 'bungalo'];
+var checkins = ['12:00', '13:00', '14:00'];
+var featuresOf = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var photosOf = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var card = {
   "author": {
@@ -24,20 +24,21 @@ var card = {
 
   "offer": {
     "title": extractRandom(titles),
-    "address": "{{location.x}}, {{location.y}}",
+    "address": '{{location.x}}, {{location.y}}',
     "price": random(1000, 1000000),
     "type": extractRandom(types),
-    "rooms": random (1, 5),
-    "guests": random (1, 100),
+    "rooms": random(1, 5),
+    "guests": random(1, 100),
     "checkin": extractRandom(checkins),
     "checkout": extractRandom(checkins),
     "features": extractRandom(featuresOf),
-    "description": "",
+    "description": '',
     "photos": extractRandom(photosOf)
   },
 
-  "location": {
-    "x": random(300, 900),
-    "y": random(150, 500)
+  'location': {
+    'x': random(300, 900),
+    'y": random(150, 500)
   }
-}
+};
+console.log(card.author.avatar);
